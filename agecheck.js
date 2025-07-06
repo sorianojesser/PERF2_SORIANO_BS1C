000ada59a4dd00f8d7b28b1c806b7a67358c156e
sorianojesser <jessersoriano49@gmail.com>
2025-07-06 19:57:44 +0900

    agecheck.js

---
+++ agecheck.js
@@ -0,0 +1,11 @@
+// ageCheck.js
+
+let age = parseInt(prompt("Enter your age:"));
+
+if (age < 13) {
+  console.log("You are a child.");
+} else if (age >= 13 && age <= 19) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
