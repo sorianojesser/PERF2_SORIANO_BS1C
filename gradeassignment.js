9f448eb40766c183dd1fe558da55b9e8c626d62f
sorianojesser <jessersoriano49@gmail.com>
2025-07-06 19:58:26 +0900

    gradeassignment.js

---
+++ gradeassignment.js
@@ -0,0 +1,13 @@
+// gradeAssignment.js
+
+let score = parseInt(prompt("Enter your score:"));
+
+if (score >= 90) {
+  console.log("Your grade is: Excellent.");
+} else if (score >= 80) {
+  console.log("Your grade is: Good.");
+} else if (score >= 70) {
+  console.log("Your grade is: Fair.");
+} else {
+  console.log("Your grade is: Needs Improvement.");
+}
